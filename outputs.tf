625c51129544cff6dec638b26d8065e81c85ddad
wmoussaoui <60008632+wmoussaoui@users.noreply.github.com>
2020-01-20 18:32:22 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,7 @@ output "vnet_subnets" {
   description = "The ids of subnets created inside the newl vNet"
   value       = "${azurerm_subnet.subnet.*.id}"
 }
+output "vnet_subnets_name" {
+  description = "The name of subnets created inside the newl vNet"
+  value       = "${azurerm_subnet.subnet.*.name}"
+}
